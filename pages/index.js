30855acccca8f20f01f95026a1d8b5e24cdaa96f
maaax.eth <maxyay5566@gmail.com>
2022-12-20 22:32:24 +0800

    update a simple title & add twitter link

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
+import twitterLogo from '../assets/twitter-logo.png';
 import { useState } from 'react';
 
 const Home = () => {
@@ -42,17 +43,17 @@ const callGenerateEndpoint = async () => {
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>Summoning Steve Jobs for your own imaginary council</h1>
+            <h1>Chat with Steve Jobs</h1>
           </div>
           <div className="header-subtitle">
-            <h2>Ask your life question to the great minds</h2>
+            <h2>Mastermind chit chat for free</h2>
           </div>
         </div>
         {/* Add prompt text box here */}
         <div className="prompt-container">
           <textarea
             className="prompt-box"
-            placeholder="start typing here"
+            placeholder="start chatting here"
             value={userInput}
             onChange={onUserChangedText}
           />;
@@ -91,6 +92,18 @@ const callGenerateEndpoint = async () => {
           <div className="badge">
             <Image src={buildspaceLogo} alt="buildspace logo" />
             <p>build with buildspace</p>
+          </div>
+        </a>
+      </div>
+      <div className="badge-container grow">
+        <a
+          href="https://twitter.com/haomaaax"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <div className="badge">
+            <Image src={twitterLogo} alt="twitter logo" />
+            <p>build by maaax</p>
           </div>
         </a>
       </div>
